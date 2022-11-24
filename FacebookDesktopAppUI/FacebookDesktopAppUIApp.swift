c747fe15269a1d1548379ae2e0d34362a0a199b7
alaxhenry <alaxhenry0121@gmail.com>
2022-11-24 17:47:28 +0900

    Initial Commit

---
+++ FacebookDesktopAppUI/FacebookDesktopAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookDesktopAppUIApp.swift
+//  FacebookDesktopAppUI
+//
+//  Created by Seungchul Ha on 2022/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FacebookDesktopAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
